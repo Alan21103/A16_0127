984c8a9a43c1d6c970ee1fb6479f97fb2ecefc2a
Alan21103 <syahlansaputra03@gmail.com>
2025-01-22 01:09:58 +0700

    Menambahkan initializer insert view model dan update aset view model

--- app/src/main/java/com/example/pamfinal/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pamfinal.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,12 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             HomeAsetViewModel(aplikasiKeuangan().container.asetRepository)
+        }
+        initializer {
+            InsertAsetViewModel(aplikasiKeuangan().container.asetRepository)
+        }
+        initializer {
+            UpdateAsetViewModel(createSavedStateHandle(), asetRepository = aplikasiKeuangan().container.asetRepository)
         }
     }
 }
