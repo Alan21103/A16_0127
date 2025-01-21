03468a1b5cd924c4f75fcb11cdbc1672288c4322
Alan21103 <syahlansaputra03@gmail.com>
2025-01-22 01:08:23 +0700

    Menambahkan fungsi insert aset view

--- app/src/main/java/com/example/pamfinal/ui/view/InsertAsetView.kt
+++ app/src/main/java/com/example/pamfinal/ui/view/InsertAsetView.kt
@@ -1,8 +1,131 @@
 package com.example.pamfinal.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pamfinal.navigation.DestinasiNavigasi
+import com.example.pamfinal.ui.customwidget.CostumeTopAppBar
+import com.example.pamfinal.ui.viewmodel.InsertAsetUiEvent
+import com.example.pamfinal.ui.viewmodel.InsertAsetUiState
+import com.example.pamfinal.ui.viewmodel.InsertAsetViewModel
+import com.example.pamfinal.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiAsetEntry : DestinasiNavigasi {
     override val route = "item_entry"
-    override val titleRes = "Entry Mhs"
+    override val titleRes = "Entry Aset"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryAsetScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertAsetViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiAsetEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryAsetBody(
+            insertUiState = viewModel.uiState,
+            onAsetValueChange = viewModel::updateInsertAstState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertAset()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryAsetBody(
+    insertUiState: InsertAsetUiState,
+    onAsetValueChange: (InsertAsetUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertAsetUiEvent,
+            onValueChange = onAsetValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@Composable
+fun FormInput(
+    insertUiEvent: InsertAsetUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertAsetUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama_aset,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama_aset = it)) },
+            label = { Text("Nama Aset") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+
+    if (enabled) {
+        Text(
+            text = "Isi Semua Data!",
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+    Divider(
+        thickness = 8.dp,
+        modifier = Modifier.padding(12.dp)
+    )
+}
+
