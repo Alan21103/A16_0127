72f5c557bfa0030ef4fc5b35db4a9eb752795102
Alan21103 <syahlansaputra03@gmail.com>
2025-01-22 01:10:28 +0700

    Membuat fungsi update aset view model

--- app/src/main/java/com/example/pamfinal/ui/viewmodel/UpdateAsetViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/viewmodel/UpdateAsetViewModel.kt
@@ -1,2 +1,42 @@
 package com.example.pamfinal.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pamfinal.repository.AsetRepository
+import com.example.pamfinal.ui.view.DestinasiAsetUpdate
+import kotlinx.coroutines.launch
+
+class UpdateAsetViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val asetRepository  : AsetRepository
+) : ViewModel(){
+
+    var uiState by mutableStateOf(InsertAsetUiState())
+        private set
+
+    val id_aset: String = checkNotNull(savedStateHandle[DestinasiAsetUpdate.id_aset])
+
+    init {
+        viewModelScope.launch {
+            uiState = asetRepository.getAsetById(id_aset).toUiStateAset()
+        }
+    }
+
+    fun updateAsetState(insertAsetUiEvent: InsertAsetUiEvent){
+        uiState = InsertAsetUiState(insertAsetUiEvent = insertAsetUiEvent)
+    }
+
+    suspend fun editAset(){
+        viewModelScope.launch {
+            try {
+                asetRepository.updateAset(id_aset, uiState.insertAsetUiEvent.toAst())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
