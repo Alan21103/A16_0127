5da217a883124bbdf60360e288c01366aa82d6ed
Alan21103 <syahlansaputra03@gmail.com>
2025-01-22 01:08:47 +0700

    Membuat fungsi insert view model

--- app/src/main/java/com/example/pamfinal/ui/viewmodel/InsertAsetViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/viewmodel/InsertAsetViewModel.kt
@@ -1,2 +1,50 @@
 package com.example.pamfinal.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pamfinal.model.Aset
+import com.example.pamfinal.repository.AsetRepository
+import kotlinx.coroutines.launch
+
+class InsertAsetViewModel(private val ast: AsetRepository) :ViewModel(){
+
+    var uiState by mutableStateOf(InsertAsetUiState())
+        private set
+
+    fun updateInsertAstState(insertAsetUiEvent: InsertAsetUiEvent){
+        uiState = InsertAsetUiState(insertAsetUiEvent = insertAsetUiEvent)
+    }
+
+    fun insertAset(){
+        viewModelScope.launch {
+            try {
+                ast.insertAset(uiState.insertAsetUiEvent.toAst())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+data class InsertAsetUiState(
+    val insertAsetUiEvent: InsertAsetUiEvent = InsertAsetUiEvent()
+)
+
+data class InsertAsetUiEvent(
+    val nama_aset: String = ""
+)
+
+fun InsertAsetUiEvent.toAst(): Aset = Aset(
+    id_aset = "",
+    nama_aset = nama_aset
+)
+
+fun Aset.toUiStateAset(): InsertAsetUiState = InsertAsetUiState(
+    insertAsetUiEvent = toInsertUiEvent()
+)
+
+fun Aset.toInsertUiEvent(): InsertAsetUiEvent = InsertAsetUiEvent(
+    nama_aset = nama_aset
+)
