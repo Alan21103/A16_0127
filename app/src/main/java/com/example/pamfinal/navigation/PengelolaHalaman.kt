2553ad9ae099cabb375679a0a814cc169fb36dc0
Alan21103 <syahlansaputra03@gmail.com>
2025-01-22 01:09:32 +0700

    Menambahkan fungsi route Entry aset dan edit aset pada pengelola halaman

--- app/src/main/java/com/example/pamfinal/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pamfinal/navigation/PengelolaHalaman.kt
@@ -3,12 +3,17 @@ package com.example.pamfinal.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.pamfinal.ui.view.DestinasiAsetEntry
+import com.example.pamfinal.ui.view.DestinasiAsetUpdate
 import com.example.pamfinal.ui.view.DestinasiHomeAset
+import com.example.pamfinal.ui.view.EntryAsetScreen
 import com.example.pamfinal.ui.view.HomeAsetScreen
+import com.example.pamfinal.ui.view.UpdateAsetView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -19,7 +24,40 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
     ) {
         composable(DestinasiHomeAset.route) {
             HomeAsetScreen(
-                navigateToItemEntry = { navController.navigate(DestinasiAsetEntry.route) }
+                navigateToItemEntry = { navController.navigate(DestinasiAsetEntry.route) },
+                navController = navController
+            )
+        }
+
+        composable(DestinasiAsetEntry.route) {
+            EntryAsetScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHomeAset.route) {
+                        popUpTo(DestinasiHomeAset.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(
+            route = DestinasiAsetUpdate.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiAsetUpdate.id_aset){
+                type = NavType.StringType
+            })
+        ) {
+            UpdateAsetView(
+                navigateBack = {
+                    navController.popBackStack()
+                },
+                onNavigateUp = {
+                    navController.navigate(
+                        DestinasiAsetUpdate.route
+                    ) {
+                        popUpTo(DestinasiHomeAset.route) {
+                            inclusive = true
+                        }
+                    }
+                }
             )
         }
     }
