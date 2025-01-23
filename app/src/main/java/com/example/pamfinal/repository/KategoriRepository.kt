afe3712c1224f58c10b4941f704a95a27a9663d5
Alan21103 <syahlansaputra03@gmail.com>
2025-01-23 16:52:57 +0700

    Membuat kategori service dan kategori repository

--- app/src/main/java/com/example/pamfinal/repository/KategoriRepository.kt
+++ app/src/main/java/com/example/pamfinal/repository/KategoriRepository.kt
@@ -1,2 +1,51 @@
 package com.example.pamfinal.repository
 
+import com.example.pamfinal.model.Kategori
+import com.example.pamfinal.service.KategoriService
+import okio.IOException
+
+interface KategoriRepository{
+    suspend fun getKategori(): List<Kategori>
+    suspend fun insertKategori(kategori: Kategori)
+    suspend fun updateKategori(id_kategori: String, kategori: Kategori)
+    suspend fun deleteKategori(id_kategori: String)
+    suspend fun getKategoriById(id_kategori: String): Kategori
+}
+
+class NetworkKategoriRepository(
+    private val kategoriApiService: KategoriService
+): KategoriRepository{
+    override suspend fun insertKategori(kategori: Kategori) {
+        kategoriApiService.insertKategori(kategori)
+    }
+
+    override suspend fun updateKategori(id_kategori: String, kategori: Kategori) {
+        kategoriApiService.updateKategori(id_kategori, kategori)
+    }
+
+    override suspend fun deleteKategori(id_kategori: String) {
+        try {
+            val response = kategoriApiService.deleteKategori(id_kategori)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete aset. HTTP Status code: ${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getKategori(): List<Kategori> = kategoriApiService.getKategori()
+
+    override suspend fun getKategoriById(id_kategori: String): Kategori {
+        try {
+            return kategoriApiService.getKategoriById(id_kategori)
+        } catch (e: IOException){
+            throw IOException("Failed to fetch aset with ID Aset: $id_kategori. Network error occurred.", e)
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/pamfinal/service/KategoriService.kt
+++ app/src/main/java/com/example/pamfinal/service/KategoriService.kt
@@ -1,4 +1,34 @@
 package com.example.pamfinal.service
 
+import com.example.pamfinal.model.Aset
+import com.example.pamfinal.model.Kategori
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
 interface KategoriService {
+    @Headers(
+        "Accept: applications/json",
+        "Content: applications/json"
+    )
+
+    @GET("bacakategori.php")
+    suspend fun getKategori(): List<Kategori>
+
+    @GET("baca1kategori.php/{id_kategori}")
+    suspend fun getKategoriById(@Query("id_kategori") id_kategori: String): Kategori
+
+    @POST("insertkategori.php")
+    suspend fun insertKategori(@Body kategori: Kategori)
+
+    @PUT("editkategori.php/{id_kategori}")
+    suspend fun updateKategori(@Query("id_kategori") id_kategori: String, @Body kategori: Kategori)
+
+    @DELETE("deletekategori.php/{id_kategori}")
+    suspend fun deleteKategori(@Query("id_kategori") id_kategori: String): Response<Void>
 }
