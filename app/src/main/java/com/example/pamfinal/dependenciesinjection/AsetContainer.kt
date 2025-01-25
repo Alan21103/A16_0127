be93c9677ce5a7a4b97b0fa7dd30a4e26aa7eb90
Alan21103 <syahlansaputra03@gmail.com>
2025-01-26 03:31:35 +0700

    Menambah pendapatan Repository dan pengeluaran repository

--- app/src/main/java/com/example/pamfinal/dependenciesinjection/AsetContainer.kt
+++ app/src/main/java/com/example/pamfinal/dependenciesinjection/AsetContainer.kt
@@ -4,8 +4,14 @@ import com.example.pamfinal.repository.AsetRepository
 import com.example.pamfinal.repository.KategoriRepository
 import com.example.pamfinal.repository.NetworkAsetRepository
 import com.example.pamfinal.repository.NetworkKategoriRepository
+import com.example.pamfinal.repository.NetworkPendapatanRepository
+import com.example.pamfinal.repository.NetworkPengeluaranRepository
+import com.example.pamfinal.repository.PendapatanRepository
+import com.example.pamfinal.repository.PengeluaranRepository
 import com.example.pamfinal.service.AsetService
 import com.example.pamfinal.service.KategoriService
+import com.example.pamfinal.service.PendapatanService
+import com.example.pamfinal.service.PengeluaranService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -14,6 +20,8 @@ import retrofit2.Retrofit
 interface AppContainer{
     val asetRepository: AsetRepository
     val kategoriRepository: KategoriRepository
+    val pendapatanRepository: PendapatanRepository
+    val pengeluaranRepository: PengeluaranRepository
 }
 
 class AsetContainer: AppContainer{
@@ -28,4 +36,10 @@ class AsetContainer: AppContainer{
 
     private val kategoriService: KategoriService by lazy { retrofit.create((KategoriService::class.java)) }
     override val kategoriRepository: KategoriRepository by lazy { NetworkKategoriRepository(kategoriService ) }
+
+    private val pendapatanService: PendapatanService by lazy { retrofit.create((PendapatanService::class.java)) }
+    override val pendapatanRepository: PendapatanRepository by lazy { NetworkPendapatanRepository(pendapatanService) }
+
+    private val pengeluaranService: PengeluaranService by lazy { retrofit.create((PengeluaranService::class.java)) }
+    override val pengeluaranRepository: PengeluaranRepository by lazy { NetworkPengeluaranRepository(pengeluaranService) }
 }
